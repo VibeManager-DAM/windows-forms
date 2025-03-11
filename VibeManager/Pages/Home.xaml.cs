978e026fd38bfebb8cafad2bd64d3435b19b771c
Juan <jabritomar@gmail.com>
2025-03-11 18:07:13 +0100

    added lateral bar navegation

--- VibeManager/Pages/Home.xaml.cs
+++ VibeManager/Pages/Home.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using VibeManager.ViewModels;
 
 namespace VibeManager.Pages
 {
-    /// <summary>
-    /// Lógica de interacción para Home.xaml
-    /// </summary>
     public partial class Home : UserControl
     {
         public Home()
         {
             InitializeComponent();
+            DataContext = new HomeVM();
+        }
+
+        private void ToggleMenu(object sender, RoutedEventArgs e)
+        {
+            ((HomeVM)DataContext).ToggleMenuCommand.Execute(null);
         }
     }
 }
--- VibeManager/ViewModels/HomeVM.cs
+++ VibeManager/ViewModels/HomeVM.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
 
 namespace VibeManager.ViewModels
 {
-    class HomeVM
+    public class HomeVM : INotifyPropertyChanged
     {
+        private object _currentView;
+        private double _sidebarWidth = 50; // Ancho inicial del menú colapsado
+        private Visibility _menuTextVisibility = Visibility.Collapsed; // Ocultar texto al inicio
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public double SidebarWidth
+        {
+            get => _sidebarWidth;
+            set { _sidebarWidth = value; OnPropertyChanged(); }
+        }
+
+        public Visibility MenuTextVisibility
+        {
+            get => _menuTextVisibility;
+            set { _menuTextVisibility = value; OnPropertyChanged(); }
+        }
+
+        public ICommand ToggleMenuCommand { get; }
+        public ICommand ShowDashboardCommand { get; }
+        public ICommand ShowUsersCommand { get; }
+        public ICommand ShowSpacesCommand { get; }
+        public ICommand ShowSettingsCommand { get; }
+
+        public HomeVM()
+        {
+            CurrentView = new DashboardVM();
+
+            ToggleMenuCommand = new RelayCommand(o => ToggleMenu());
+            ShowDashboardCommand = new RelayCommand(o => CurrentView = new DashboardVM());
+            ShowUsersCommand = new RelayCommand(o => CurrentView = new UsersVM());
+            ShowSpacesCommand = new RelayCommand(o => CurrentView = new SpacesVM());
+            ShowSettingsCommand = new RelayCommand(o => CurrentView = new SettingsVM());
+        }
+
+        private void ToggleMenu()
+        {
+            if (SidebarWidth == 50)
+            {
+                SidebarWidth = 200;
+                MenuTextVisibility = Visibility.Visible;
+            }
+            else
+            {
+                SidebarWidth = 50;
+                MenuTextVisibility = Visibility.Collapsed;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
